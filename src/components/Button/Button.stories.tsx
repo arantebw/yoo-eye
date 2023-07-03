e636339a6bd377e2c59e319d811d973a4e8589eb
Billy Arante <arantebillywilson@gmail.com>
2023-07-04 01:25:04 +0800

    feat(components/button): create the initial button story

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+import { ButtonProps } from "./Button.types";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: "Button",
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = (args: ButtonProps) => <Button {...args} />;
+Primary.args = {
+  text: "Primary",
+};
