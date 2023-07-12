9ccd95bbf1910456ae3d394167f13b3befe9aef6
Billy Arante <arantebillywilson@gmail.com>
2023-07-12 22:14:27 +0800

    chore(rollup): update rollup configs

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,13 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import terser from "@rollup/plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
 export default [
   {
     input: "src/index.ts",
+    external: ["react-dom"],
     output: [
       {
         file: packageJson.main,
@@ -23,17 +22,14 @@ export default [
       },
     ],
     plugins: [
-      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
     ],
-    external: ["react", "react-dom", "styled-components"],
   },
   {
-    input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "es" }],
-    plugins: [dts.default()],
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
   },
 ];
