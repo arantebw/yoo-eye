55f3fe616f44b2f2b95663b3a60b75f25001572f
Billy Arante <arantebillywilson@gmail.com>
2023-07-16 23:01:24 +0800

    feat(storybook): ✨ customize and configure storybook

---
+++ .storybook/yooEyeTheme.js
@@ -0,0 +1,10 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Yoo-Eye Components",
+  brandUrl: "https://www.npmjs.com/package/@arantebw/yoo-eye",
+  brandImage:
+    "https://res.cloudinary.com/dsmhiu5fj/image/upload/f_auto,q_auto/gi2e4mtwhqxqollxgm0h",
+  brandTarget: "_self",
+});
