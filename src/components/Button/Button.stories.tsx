96eb8d106d125055c506b9e21bb0ce3df20d7d1b
Billy Arante <arantebillywilson@gmail.com>
2023-07-18 23:40:02 +0800

    refactor(components/button): use the args field to add controls to the component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import Button from "./Button";
+import { ButtonProps } from "./Button.types";
 
 const meta: Meta<typeof Button> = {
   component: Button,
@@ -11,6 +12,30 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const SolidButton: Story = {
-  render: () => <Button variant="solid">Solid</Button>,
+export const SolidButton: Story = (args: ButtonProps) => (
+  <Button variant="solid">Solid</Button>
+);
+SolidButton.args = {
+  variant: "solid",
+};
+
+export const GhostButton: Story = (args: ButtonProps) => (
+  <Button variant="ghost">Ghost</Button>
+);
+GhostButton.args = {
+  variant: "ghost",
+};
+
+export const SoftButton: Story = (args: ButtonProps) => (
+  <Button {...args}>Soft</Button>
+);
+SoftButton.args = {
+  variant: "soft",
+};
+
+export const TransparentButton: Story = (args: ButtonProps) => (
+  <Button {...args}>Transparent</Button>
+);
+TransparentButton.args = {
+  variant: "transparent",
 };
